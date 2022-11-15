a7314a2173bd96e4a4ef470a5d07d7c48673ea12
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-11-14 23:01:38 -0700

    feedback

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-let numbers = document.getElementsByClassName("number");
-let submitBtn = document.getElementById("submit");
-let cards = document.querySelectorAll(".card");
+let form = document.getElementById("form");
+let selected = document.getElementById("selected-text");
+let steps = document.getElementsByClassName("step");
 
-[...numbers].forEach((btn) => {
-  btn.addEventListener("click", (evt) => {
-    [...numbers].map((btn) => btn.classList.remove("selected"));
-    btn.classList.add("selected");
-  });
-});
-
-submitBtn.addEventListener("click", () => {
-  let selected = document.querySelector(".number.selected");
-  if (!selected) {
-    alert("Select a rating!");
-    return;
-  }
-  document.getElementById(
-    "selected-text"
-  ).innerText = `You selected ${selected.innerText} out of ${numbers.length}`;
-
-  [...cards].map((card) => card.classList.toggle("hidden"));
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  value = evt.target.rating.value;
+  if (!value) return;
+  selected.innerText = `You selected ${value} out of 5`;
+  [...steps].map((step) => step.classList.toggle("hidden"));
 });
